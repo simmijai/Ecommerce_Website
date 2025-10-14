0aaa5ab33336afe94177ba4b74738096bc7b323c
simmijai <109519581+simmijai@users.noreply.github.com>
2025-10-14 15:07:22 +0530

    Address pages Bootstrap Added and wishlist page image problem solved

--- orders/forms.py
+++ orders/forms.py
@@ -5,6 +5,6 @@ class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
         fields = [
-            'full_name', 'phone', 'address_line_1', 'address_line_2',
+            'full_name', 'phone', 'address_line_1',
             'city', 'state', 'country', 'pincode', 'address_type', 'is_default'
         ]
